4b1e57c8309ac955bac5fdac21d0581e134cd1ad
Johl Brown <johlbrown@gmail.com>
2026-08-04 21:53:19 +1000

    Scan finite linear-integral realization

--- scripts/check_ns_luo_physical_analytic_frontier.sh
+++ scripts/check_ns_luo_physical_analytic_frontier.sh
@@ -13,6 +13,7 @@ FILES=(
   DASHI/Physics/Closure/NSTriadKNLuoFiniteCharacterWeightedIncrementExact.agda
   DASHI/Physics/Closure/NSTriadKNLuoFiniteCharacterMultiplierBridgeExact.agda
   DASHI/Physics/Closure/NSTriadKNLuoLinearIntegralWeightedIncrementExact.agda
+  DASHI/Physics/Closure/NSTriadKNLuoFiniteLinearIntegralRealizationExact.agda
   DASHI/Physics/Closure/NSTriadKNLuoFiniteTranslationTensorConvolutionExact.agda
   DASHI/Physics/Closure/NSTriadKNLuoFiniteResonantPairFoldExact.agda
   DASHI/Physics/Closure/NSTriadKNLuoFiniteWeightedIncrementFubiniExact.agda
