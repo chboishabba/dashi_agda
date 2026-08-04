d9950b1c62057ffa62f169eb66c70f5a4f15b957
Johl Brown <johlbrown@gmail.com>
2026-08-04 22:45:15 +1000

    Extend P33 local checker through SU2, covariant, inverse, and Neumann reductions

--- scripts/check_yang_mills_p33_five_local_bounds.sh
+++ scripts/check_yang_mills_p33_five_local_bounds.sh
@@ -10,11 +10,18 @@ files=(
   DASHI/Physics/YangMills/BalabanP33LiteralBondCellIncidenceExact.agda
   DASHI/Physics/YangMills/BalabanP33PrimitiveOperatorNormLocalBoundsExact.agda
   DASHI/Physics/YangMills/BalabanP33PrimitiveAbsoluteOperatorAdapterExact.agda
+  DASHI/Physics/YangMills/BalabanP33SU2QuadraticPrimitiveNormAdapterExact.agda
+  DASHI/Physics/YangMills/BalabanP33LiteralCovariantDerivativeDifferenceExact.agda
+  DASHI/Physics/YangMills/BalabanP33LiteralCovariantDivergenceDifferenceExact.agda
   DASHI/Physics/YangMills/BalabanP33SignedFiniteAtomExpansionExact.agda
+  DASHI/Physics/YangMills/BalabanP33AbsoluteFiniteAtomAdapterExact.agda
   DASHI/Physics/YangMills/BalabanP33ConfiguredSignedAtomListsExact.agda
+  DASHI/Physics/YangMills/BalabanP33FiveSandwichSignedFormExact.agda
+  DASHI/Physics/YangMills/BalabanP33SandwichLocalFamilyExact.agda
   DASHI/Physics/YangMills/BalabanP33IdentityCurvatureLocalExact.agda
   DASHI/Physics/YangMills/BalabanP33LiteralFiveMechanismFamiliesExact.agda
   DASHI/Physics/YangMills/BalabanP33RationalInverseNorm32Exact.agda
+  DASHI/Physics/YangMills/BalabanP33WeightedNeumannHalfContractionExact.agda
   DASHI/Physics/YangMills/BalabanP33FiveLocalPhysicalBoundsValidation.agda
 )
 
@@ -29,29 +36,41 @@ fi
 
 grep -q 'bondCellChargeSumExact' \
   DASHI/Physics/YangMills/BalabanP33LiteralBondCellIncidenceExact.agda
-grep -q 'curvatureCoefficient dataSet cell' \
-  DASHI/Physics/YangMills/BalabanP33PrimitiveOperatorNormLocalBoundsExact.agda
 grep -q 'transportCoefficientBound' \
   DASHI/Physics/YangMills/BalabanP33PrimitiveOperatorNormLocalBoundsExact.agda
 grep -q 'operatorNormDominatesCoordinate' \
   DASHI/Physics/YangMills/BalabanP33PrimitiveAbsoluteOperatorAdapterExact.agda
+grep -q 'dexpPairNormBelowTwoRadius' \
+  DASHI/Physics/YangMills/BalabanP33SU2QuadraticPrimitiveNormAdapterExact.agda
+grep -q 'covariantDerivativeDifferenceExact' \
+  DASHI/Physics/YangMills/BalabanP33LiteralCovariantDerivativeDifferenceExact.agda
+grep -q 'covariantDivergenceDifferenceExact' \
+  DASHI/Physics/YangMills/BalabanP33LiteralCovariantDivergenceDifferenceExact.agda
 grep -q 'sumMappedTwoSided' \
   DASHI/Physics/YangMills/BalabanP33SignedFiniteAtomExpansionExact.agda
+grep -q 'fromAbsoluteFixedAtomExpansion' \
+  DASHI/Physics/YangMills/BalabanP33AbsoluteFiniteAtomAdapterExact.agda
 grep -q 'configuredSignedAtomsGivePath4PhysicalCoercivity' \
   DASHI/Physics/YangMills/BalabanP33ConfiguredSignedAtomListsExact.agda
+grep -q 'constraintSignedFormBound' \
+  DASHI/Physics/YangMills/BalabanP33FiveSandwichSignedFormExact.agda
+grep -q 'localSandwichRemainderBound' \
+  DASHI/Physics/YangMills/BalabanP33SandwichLocalFamilyExact.agda
 grep -q 'identityCurvatureCellExact' \
   DASHI/Physics/YangMills/BalabanP33IdentityCurvatureLocalExact.agda
 grep -q 'literalFiveMechanismsGivePath4PhysicalCoercivity' \
   DASHI/Physics/YangMills/BalabanP33LiteralFiveMechanismFamiliesExact.agda
 grep -q 'p33InverseNormAtMostThirtyTwo' \
   DASHI/Physics/YangMills/BalabanP33RationalInverseNorm32Exact.agda
+grep -q 'weightedResidualHalfPowerBound' \
+  DASHI/Physics/YangMills/BalabanP33WeightedNeumannHalfContractionExact.agda
 
 grep -q '10.1007/BF01240355' \
-  DASHI/Physics/YangMills/BalabanP33LiteralFiveMechanismFamiliesExact.agda
+  DASHI/Physics/YangMills/BalabanP33LiteralCovariantDivergenceDifferenceExact.agda
 grep -q '10.1007/BF01211042' \
   DASHI/Physics/YangMills/BalabanP33ConfiguredSignedAtomListsExact.agda
 grep -q '10.1007/978-3-319-13467-3' \
-  DASHI/Physics/YangMills/BalabanP33PrimitiveOperatorNormLocalBoundsExact.agda
+  DASHI/Physics/YangMills/BalabanP33SU2QuadraticPrimitiveNormAdapterExact.agda
 grep -q '10.1007/978-3-642-66282-9' \
   DASHI/Physics/YangMills/BalabanP33PrimitiveAbsoluteOperatorAdapterExact.agda
 grep -q '10.1017/CBO9781139020411' \
