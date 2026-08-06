4bebb33b5671cf2f432c359edc0367ca96788faa
Johl Brown <johlbrown@gmail.com>
2026-08-06 20:12:42 +1000

    Check finite torus translation bridge in Round 7

--- scripts/check_conscious_access_round7_harmonic_wreath_carrier.sh
+++ scripts/check_conscious_access_round7_harmonic_wreath_carrier.sh
@@ -13,7 +13,9 @@ files=(
   DASHI/Physics/Common/ScaledFilteredEstimateExact.agda
   DASHI/Physics/Common/SeparatingProbeFamilyExact.agda
   DASHI/Physics/Common/FiniteWreathRefinementExact.agda
+  DASHI/Physics/Common/FiniteThreeCycleTorusExact.agda
   DASHI/Biology/BalancedTernaryHarmonicCarrierExact.agda
+  DASHI/Biology/BalancedTernaryTorusWreathBridgeExact.agda
   DASHI/Biology/BalancedTernaryTetrationHarmonicBridgeExact.agda
   DASHI/Biology/JFineCoarseRelativeScaleExact.agda
   DASHI/Biology/MonsterStructuredBulkCarrierExact.agda
@@ -22,9 +24,7 @@ files=(
   DASHI/Biology/ConsciousAccessRound7HarmonicWreathCarrierValidation.agda
 )
 
-for file in "${files[@]}"; do
-  test -f "$file"
-done
+for file in "${files[@]}"; do test -f "$file"; done
 
 if grep -nE '(^|[[:space:]])postulate([[:space:]]|$)|\{!|!\}|TERMINATING|NO_TERMINATION_CHECK|allow-unsolved-metas|--no-positivity-check|--no-termination-check|NON_COVERING|--type-in-type|trustMe|primTrustMe' "${files[@]}"; then
   echo "round seven contains a hole, postulate, unsafe escape, or trust primitive" >&2
@@ -38,9 +38,16 @@ checks=(
   'ScaledFilteredEstimateExact.agda:lossCorrectedTransportExact'
   'SeparatingProbeFamilyExact.agda:pairProbesSeparate'
   'FiniteWreathRefinementExact.agda:localPermutationStepTwiceAt'
+  'FiniteThreeCycleTorusExact.agda:translateFirstCubed'
+  'FiniteThreeCycleTorusExact.agda:translateSecondCubed'
+  'FiniteThreeCycleTorusExact.agda:translationsCommute'
+  'FiniteThreeCycleTorusExact.agda:pullbacksCommuteAt'
   'BalancedTernaryHarmonicCarrierExact.agda:coarseChannelCountIsTen'
   'BalancedTernaryHarmonicCarrierExact.agda:jFine'
   'BalancedTernaryHarmonicCarrierExact.agda:elementaryHarmonicDimensionIs196830'
+  'BalancedTernaryTorusWreathBridgeExact.agda:decodeEncodeChannel'
+  'BalancedTernaryTorusWreathBridgeExact.agda:completionJFixedByFirstTranslation'
+  'BalancedTernaryTorusWreathBridgeExact.agda:completionJFixedBySecondTranslation'
   'BalancedTernaryTetrationHarmonicBridgeExact.agda:jFineEvaluation'
   'BalancedTernaryTetrationHarmonicBridgeExact.agda:existingTowerLevelTwoHasNineToNineSites'
   'BalancedTernaryTetrationHarmonicBridgeExact.agda:recursiveSelfIndexingTetration'
@@ -66,6 +73,7 @@ for check in "${checks[@]}"; do
 done
 
 grep -q '10.1017/CBO9780511626265' DASHI/Physics/Common/FiniteRingScaleDualityExact.agda
+grep -q '10.1017/CBO9780511626265' DASHI/Physics/Common/FiniteThreeCycleTorusExact.agda
 grep -q '10.1137/1.9781611970104' DASHI/Biology/BalancedTernaryHarmonicCarrierExact.agda
 grep -q '10.1090/surv/117' DASHI/Biology/BalancedTernaryTetrationHarmonicBridgeExact.agda
 grep -q '10.1007/978-1-4612-0731-3' DASHI/Physics/Common/FiniteWreathRefinementExact.agda
@@ -73,6 +81,7 @@ grep -q '10.1017/S1446788700031323' DASHI/Biology/MonsterSubgroupBranchingBenchm
 grep -q '10.48550/arXiv.2412.12182' DASHI/Biology/MonsterSubgroupBranchingBenchmarksExact.agda
 grep -q 'tenIsPrimitiveSymmetryMultiplicityIsFalse' DASHI/Biology/MonsterFilteredCarrierExact.agda
 grep -q 'elementaryChannelCarrierDoesNotEqualFullAssignmentSpace' DASHI/Biology/BalancedTernaryHarmonicCarrierExact.agda
+grep -q 'jIsNotTenthOrdinaryTorusResidue' DASHI/Biology/BalancedTernaryTorusWreathBridgeExact.agda
 grep -q 'elementaryChannelsAreNotFullConfigurations' DASHI/Biology/BalancedTernaryTetrationHarmonicBridgeExact.agda
 grep -q 'structuredChannelsAreNotClaimedMonsterInvariantSummands' DASHI/Biology/MonsterStructuredBulkCarrierExact.agda
 
