cb49aadb332ba43f4113f32859d4d1ddb3dcd659
Johl Brown <johlbrown@gmail.com>
2026-08-06 19:05:36 +1000

    Scan the dependency-ordered constant-one Holder proof

--- scripts/check_ns_luo_physical_carrier_round16.sh
+++ scripts/check_ns_luo_physical_carrier_round16.sh
@@ -8,7 +8,7 @@ bash scripts/check_ns_luo_galerkin_kernel_round15.sh
 
 FILES=(
   DASHI/Physics/Closure/NSTriadKNLuoLowHighExponentBookkeepingCorrectedExact.agda
-  DASHI/Physics/Closure/NSTriadKNLuoFiniteSixThreeHolderExact.agda
+  DASHI/Physics/Closure/NSTriadKNLuoFiniteSixThreeHolderConstantOneExact.agda
   DASHI/Physics/Closure/NSTriadKNLuoFiniteSixThreeHolderDimensionFreeExact.agda
   DASHI/Physics/Closure/NSTriadKNLuoFiniteSixThreeKernelDimensionFreeExact.agda
   DASHI/Physics/Closure/NSTriadKNLuoFiniteCenteredDifferenceSixThreeExact.agda
