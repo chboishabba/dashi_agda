98ca02758bb6153b099623aced4177ab91dd6eb8
Johl Brown <johlbrown@gmail.com>
2026-08-29 21:08:39 +1000

    Check finite spectral rotation affine and sensitivity producers

--- scripts/check_relation_representation_realization.sh
+++ scripts/check_relation_representation_realization.sh
@@ -10,6 +10,7 @@ FILES=(
   DASHI/Reasoning/RelationRepresentationRealizationExact.agda
   DASHI/Reasoning/BidirectionalRelationRepresentationBridgeExact.agda
   DASHI/Reasoning/RelationRepresentationExperimentProtocolExact.agda
+  DASHI/Reasoning/FiniteRelationLinearAlgebraProducerExact.agda
   DASHI/Reasoning/EigenslurFlourishingRelationBoundaryExact.agda
   DASHI/Reasoning/RelationRepresentationCrossPollinationExact.agda
   DASHI/Reasoning/HumourRelationRepresentationCrossPollinationExact.agda
@@ -35,47 +36,28 @@ grep -q '10.48550/arXiv.2601.16907' DASHI/Reasoning/RelationRepresentationSource
 grep -q '10.48550/arXiv.2606.01402' DASHI/Reasoning/RelationRepresentationSourceRegistryExact.agda
 grep -q '10.48550/arXiv.2602.02859' DASHI/Reasoning/RelationRepresentationSourceRegistryExact.agda
 
-grep -q 'manifoldConstrainedTransformation' DASHI/Reasoning/RelationRepresentationAdequacyExact.agda
-grep -q 'ordinalConcordanceGeometry' DASHI/Reasoning/RelationRepresentationAdequacyExact.agda
-grep -q 'magnitudeAwareGeometry' DASHI/Reasoning/RelationRepresentationAdequacyExact.agda
-grep -q 'calibratedMonotoneGeometry' DASHI/Reasoning/RelationRepresentationAdequacyExact.agda
 grep -q 'sameRetainedRelationStateGivesSameObservationAfterEveryTrace' DASHI/Reasoning/RelationRepresentationAdequacyExact.agda
-grep -q 'relationRepresentationCommutesWithEveryDeclaredTrace' DASHI/Reasoning/RelationRepresentationAdequacyExact.agda
-
 grep -q 'RepresentationRealizationWitness' DASHI/Reasoning/RelationRepresentationRealizationExact.agda
-grep -q 'representationCollisionBlocksRealization' DASHI/Reasoning/RelationRepresentationRealizationExact.agda
-grep -q 'propertyCodeCannotRealizePreciseRelation' DASHI/Reasoning/RelationRepresentationRealizationExact.agda
-grep -q 'compactRelationCannotRealizeSituatedMeaning' DASHI/Reasoning/RelationRepresentationRealizationExact.agda
-
-grep -q 'BidirectionalRelationBridge' DASHI/Reasoning/BidirectionalRelationRepresentationBridgeExact.agda
 grep -q 'collisionReopensBidirectionalCut' DASHI/Reasoning/BidirectionalRelationRepresentationBridgeExact.agda
-grep -q 'bothReceiptsMustUseSameEncoding' DASHI/Reasoning/BidirectionalRelationRepresentationBridgeExact.agda
-
-grep -q 'CandidateConsumerFailure' DASHI/Reasoning/RelationRepresentationExperimentProtocolExact.agda
 grep -q 'ReopenedCandidateSearch' DASHI/Reasoning/RelationRepresentationExperimentProtocolExact.agda
-grep -q 'targetHeldOut' DASHI/Reasoning/RelationRepresentationExperimentProtocolExact.agda
-grep -q 'temporalCheckpointHeldOut' DASHI/Reasoning/RelationRepresentationExperimentProtocolExact.agda
-grep -q 'collisionReopensCandidateSearch' DASHI/Reasoning/RelationRepresentationExperimentProtocolExact.agda
+
+grep -q '10.1017/CBO9781139020411' DASHI/Reasoning/FiniteRelationLinearAlgebraProducerExact.agda
+grep -q 'principalEigenpair' DASHI/Reasoning/FiniteRelationLinearAlgebraProducerExact.agda
+grep -q 'rankOneOuterProductReceipt' DASHI/Reasoning/FiniteRelationLinearAlgebraProducerExact.agda
+grep -q 'mismatchRankOneObstruction' DASHI/Reasoning/FiniteRelationLinearAlgebraProducerExact.agda
+grep -q 'quarterTurnFourth' DASHI/Reasoning/FiniteRelationLinearAlgebraProducerExact.agda
+grep -q 'affineDemoAt23' DASHI/Reasoning/FiniteRelationLinearAlgebraProducerExact.agda
+grep -q 'localSensitivityChangesWithState' DASHI/Reasoning/FiniteRelationLinearAlgebraProducerExact.agda
+grep -q 'ManifoldProducerObligation' DASHI/Reasoning/FiniteRelationLinearAlgebraProducerExact.agda
 
 grep -q 'functioningDoesNotRecoverCapability' DASHI/Reasoning/EigenslurFlourishingRelationBoundaryExact.agda
-grep -q 'coarsePositiveCodeCannotRealizeAgency' DASHI/Reasoning/EigenslurFlourishingRelationBoundaryExact.agda
-grep -q 'flourishingRequiresRankOneDirection' DASHI/Reasoning/EigenslurFlourishingRelationBoundaryExact.agda
-
-grep -q 'humourSourceDoesNotClaimEpistemicAgency' DASHI/Reasoning/HumourRelationRepresentationCrossPollinationExact.agda
-grep -q 'corpusLoomRetainsHumourSourceBeforeDialecticalCorrection' DASHI/Reasoning/HumourRelationRepresentationCrossPollinationExact.agda
-grep -q 'humourDynamicConsumerSafetyAsymmetry' DASHI/Reasoning/HumourRelationRepresentationCrossPollinationExact.agda
 grep -q 'oneHumourConsumerSafetyDoesNotEstablishPluralSafety' DASHI/Reasoning/HumourRelationRepresentationCrossPollinationExact.agda
-
-grep -q 'neuralCoarseCompressionLosesVariation' DASHI/Reasoning/NeuralSpectralRelationCrossPollinationExact.agda
 grep -q 'neuralSearchMayReopenFromOffsetToRotation' DASHI/Reasoning/NeuralSpectralRelationCrossPollinationExact.agda
-grep -q 'cleanupRetainsSymmetryAdaptedModes' DASHI/Reasoning/NeuralSpectralRelationCrossPollinationExact.agda
-
 grep -q 'grokkingCurrentFitDoesNotCloseLearningFuture' DASHI/Reasoning/RelationRepresentationCrossPollinationExact.agda
-grep -q 'equalAmplitudeDoesNotCollapsePhase' DASHI/Reasoning/RelationRepresentationRegression.agda
-grep -q 'humourCurrentPositiveSurfaceDoesNotEstablishPluralFutureSafety' DASHI/Reasoning/RelationRepresentationRegression.agda
-grep -q 'neuralConsumerCollisionReopensCandidateSearch' DASHI/Reasoning/RelationRepresentationRegression.agda
+grep -q 'finiteDifferenceSensitivityIsStateDependent' DASHI/Reasoning/RelationRepresentationRegression.agda
 
 if command -v agda >/dev/null 2>&1; then
+  agda -i . -i src DASHI/Reasoning/FiniteRelationLinearAlgebraProducerExact.agda
   agda -i . -i src DASHI/Reasoning/RelationRepresentationRegression.agda
   agda -i . -i src DASHI/Reasoning/Everything.agda
 else
