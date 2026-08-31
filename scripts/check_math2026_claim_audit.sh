dfa66649c9e6f82812ba553546e733591a4431c7
Johl Brown <johlbrown@gmail.com>
2026-08-31 21:41:08 +1000

    Checker: cover new cross-pollination owners

--- scripts/check_math2026_claim_audit.sh
+++ scripts/check_math2026_claim_audit.sh
@@ -5,11 +5,17 @@ ROOT="DASHI/Math2026ClaimAuditValidation.agda"
 FILES=(
   DASHI/Analysis/DeBruijnNewman2026ClaimAuditExact.agda
   DASHI/Analysis/DeBruijnNewman2026SourceWeldExact.agda
+  DASHI/Analysis/DeBruijnNewmanRiemannG2BridgeAuditExact.agda
   DASHI/Mathematics/NumberTheory/PrimeGap2026ClaimAuditExact.agda
+  DASHI/Mathematics/NumberTheory/PrimeGap2026SourceAcquisitionExact.agda
   DASHI/Mathematics/NumberTheory/DiophantineTupleDPrimeSquare2026ClaimAuditExact.agda
   DASHI/Mathematics/NumberTheory/PrimePowerDiophantineTuple2026SourceExact.agda
   DASHI/Core/ExternalAutoformalizationProvenanceExact.agda
   DASHI/Core/SourceExactFrontierBidiCrossPollination2026.agda
+  DASHI/Core/FrontierRelationStrengthBidiExact.agda
+  DASHI/Core/FiniteCertificateConsumerBridgeExact.agda
+  DASHI/Physics/YangMills/BalabanActiveSourceDiscriminator2026Exact.agda
+  DASHI/Physics/Closure/NSCriticalConeAristotleRouteHypergraph2026Exact.agda
   "$ROOT"
 )
 
