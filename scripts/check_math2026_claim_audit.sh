2f6f8e9727c9081ce328f15d1f9bcc6fef3d04b0
Johl Brown <johlbrown@gmail.com>
2026-08-31 23:41:54 +1000

    Checker: include simultaneous C3 determinant covariance

--- scripts/check_math2026_claim_audit.sh
+++ scripts/check_math2026_claim_audit.sh
@@ -15,6 +15,7 @@ FILES=(
   DASHI/Analysis/RiemannG2PostSchurDeterminantSufficiency369Exact.agda
   DASHI/Analysis/RiemannG2DeterminantSymmetryRelevance369Exact.agda
   DASHI/Analysis/RiemannG2C3FixedNuisanceDeterminantNoGo369Exact.agda
+  DASHI/Analysis/RiemannG2C3SimultaneousDeterminantCovariance369Exact.agda
   DASHI/Mathematics/NumberTheory/PrimeGap2026ClaimAuditExact.agda
   DASHI/Mathematics/NumberTheory/PrimeGap2026SourceAcquisitionExact.agda
   DASHI/Mathematics/NumberTheory/DiophantineTupleDPrimeSquare2026ClaimAuditExact.agda
