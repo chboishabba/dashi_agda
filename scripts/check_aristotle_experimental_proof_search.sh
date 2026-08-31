14f679894756315c1e71d4c6a1e19d5aaf5e96cd
Johl Brown <johlbrown@gmail.com>
2026-08-31 20:20:27 +1000

    RH: trust-scan bidi scheduler regression

--- scripts/check_aristotle_experimental_proof_search.sh
+++ scripts/check_aristotle_experimental_proof_search.sh
@@ -10,6 +10,7 @@ FILES=(
   DASHI/Analysis/RiemannAristotlePoleQuotientLeanReturn8889Exact.agda
   DASHI/Analysis/RiemannAristotleExperimentalProofSearchExact.agda
   DASHI/Analysis/RiemannAristotleRHBidiSearchSchedulerExact.agda
+  DASHI/Analysis/RiemannAristotleRHBidiSearchSchedulerRegression.agda
   DASHI/Reasoning/AristotleExperimentalProofSearchValidation.agda
 )
 
