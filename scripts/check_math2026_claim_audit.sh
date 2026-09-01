9c51efd0f449534736e915ff83d83bd645ed3108
Johl Brown <johlbrown@gmail.com>
2026-09-01 16:27:05 +1000

    Trust-scan single-producer legacy H_A compiler

--- scripts/check_math2026_claim_audit.sh
+++ scripts/check_math2026_claim_audit.sh
@@ -51,6 +51,7 @@ FILES=(
   DASHI/Analysis/RiemannG2HAConsumerWeightedRecoveryTraceExact.agda
   DASHI/Analysis/RiemannG2HASourceProducerHyperedgeExact.agda
   DASHI/Analysis/RiemannG2HASingleSourceProducerBidiExact.agda
+  DASHI/Analysis/RiemannG2HASingleProducerLegacyHACompilerExact.agda
   DASHI/Mathematics/NumberTheory/PrimeGap2026ClaimAuditExact.agda
   DASHI/Mathematics/NumberTheory/PrimeGap2026SourceAcquisitionExact.agda
   DASHI/Mathematics/NumberTheory/DiophantineTupleDPrimeSquare2026ClaimAuditExact.agda
