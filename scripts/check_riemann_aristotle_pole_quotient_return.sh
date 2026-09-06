bbb0b7e741da8b09f9838a4453744404ef9339f5
Johl Brown <johlbrown@gmail.com>
2026-09-07 02:00:00 +1000

    Check final pole-quotient allowance compiler

--- scripts/check_riemann_aristotle_pole_quotient_return.sh
+++ scripts/check_riemann_aristotle_pole_quotient_return.sh
@@ -8,6 +8,7 @@ FILES=(
   DASHI/Analysis/RiemannAristotlePoleQuotientComplementMarginCompilerExact.agda
   DASHI/Analysis/RiemannAristotlePoleQuotientBidiMeetingExact.agda
   DASHI/Analysis/RiemannAristotlePoleQuotientSplitComplementBudgetExact.agda
+  DASHI/Analysis/RiemannG2PoleQuotientChannelAllowanceExact.agda
   DASHI/Analysis/RiemannAristotleRankTwoPoleQuotientLaneSeparationExact.agda
   DASHI/Analysis/RiemannAristotlePoleQuotientOffOrdinateBudgetTargetExact.agda
   DASHI/Analysis/RiemannAristotlePoleQuotientGammaBudgetTargetExact.agda
@@ -29,6 +30,7 @@ done
 if command -v agda >/dev/null 2>&1; then
   agda DASHI/Analysis/RiemannAristotlePoleQuotientComplementMarginCompilerExact.agda
   agda DASHI/Analysis/RiemannAristotlePoleQuotientSplitComplementBudgetExact.agda
+  agda DASHI/Analysis/RiemannG2PoleQuotientChannelAllowanceExact.agda
   agda DASHI/Analysis/RiemannAristotlePoleQuotientBidiMeetingExact.agda
   agda DASHI/Analysis/RiemannAristotleRankTwoPoleQuotientLaneSeparationExact.agda
   agda DASHI/Analysis/RiemannAristotlePoleQuotientOffOrdinateBudgetTargetExact.agda
