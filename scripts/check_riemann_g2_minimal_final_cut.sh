cf372380febac744b0f0524afc014d74f959586a
Johl Brown <johlbrown@gmail.com>
2026-09-08 05:26:16 +1000

    RH: validate one-leaf complement route

--- scripts/check_riemann_g2_minimal_final_cut.sh
+++ scripts/check_riemann_g2_minimal_final_cut.sh
@@ -27,6 +27,9 @@ FILES=(
   DASHI/Analysis/RiemannG2BudgetNormalizedAnalyticCoresExact.agda
   DASHI/Analysis/RiemannG2BudgetNormalizedFinalOrderTransportExact.agda
   DASHI/Analysis/RiemannG2UniformBudgetNormalizedHighProducerExact.agda
+  DASHI/Analysis/RiemannG2LiteralResponseNormalizedAnalyticCoresExact.agda
+  DASHI/Analysis/RiemannG2IndependentComplementMarginFinalExact.agda
+  DASHI/Analysis/RiemannG2UniformIndependentComplementHighProducerExact.agda
   DASHI/Analysis/RiemannG2FinalSplitComplementOrderTransportCompilerExact.agda
   DASHI/Analysis/RiemannG2FinalPoleQuotientMinimalAnalyticCutExact.agda
   DASHI/Analysis/RiemannG2FinalPoleQuotientTwoPaymentCutExact.agda
