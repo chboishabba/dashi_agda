107fac421ccd1a75701488aad42cdb221e2f9a6f
Johl Brown <johlbrown@gmail.com>
2026-09-09 14:43:14 +1000

    Compile bending torsion laws in textile physics gate

--- scripts/check_textile_physics.sh
+++ scripts/check_textile_physics.sh
@@ -10,6 +10,7 @@ AGDA_STDLIB="${AGDA_STDLIB:-/usr/share/agda/lib/stdlib}"
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Textile/QuasiStaticTextileLoadTransferExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Textile/TextileFailureSlipPredicateExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Textile/FiniteTextileEquilibriumNetworkExact.agda
+"$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Textile/BendingTorsionTextileLawExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Topology/TextileStitchHyperfabricExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Topology/TextileStitchOperationalSemanticsExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Topology/CircularKnittingHelicalLoopBridgeExact.agda
