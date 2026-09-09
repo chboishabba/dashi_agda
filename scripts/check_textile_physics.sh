567e076ba62ab0eac128f0b9ed3d8093a869ab13
Johl Brown <johlbrown@gmail.com>
2026-09-09 14:26:34 +1000

    Add circular knit bridge to textile compile gate

--- scripts/check_textile_physics.sh
+++ scripts/check_textile_physics.sh
@@ -8,6 +8,7 @@ AGDA_STDLIB="${AGDA_STDLIB:-/usr/share/agda/lib/stdlib}"
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Textile/TextileMechanicalFibreExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Topology/TextileStitchHyperfabricExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Topology/TextileStitchOperationalSemanticsExact.agda
+"$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Topology/CircularKnittingHelicalLoopBridgeExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Textile/JacquardPhysicalFibreBridgeExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Textile/StitchPhysicalFibreBridgeExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/TextilePhysicsValidation.agda
