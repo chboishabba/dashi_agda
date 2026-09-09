cd170dbf84f014a2111012a31688e6c1f1206e68
Johl Brown <johlbrown@gmail.com>
2026-09-09 14:27:40 +1000

    Add crochet micro semantics to textile compile gate

--- scripts/check_textile_physics.sh
+++ scripts/check_textile_physics.sh
@@ -9,6 +9,7 @@ AGDA_STDLIB="${AGDA_STDLIB:-/usr/share/agda/lib/stdlib}"
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Topology/TextileStitchHyperfabricExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Topology/TextileStitchOperationalSemanticsExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Topology/CircularKnittingHelicalLoopBridgeExact.agda
+"$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Topology/CrochetHookMicroSemanticsExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Textile/JacquardPhysicalFibreBridgeExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Textile/StitchPhysicalFibreBridgeExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/TextilePhysicsValidation.agda
