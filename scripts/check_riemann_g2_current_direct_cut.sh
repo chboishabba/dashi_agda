d3b359d9a2304435bb6687ac0d6c43bbe94b248c
Johl Brown <johlbrown@gmail.com>
2026-09-09 23:33:07 +1000

    RH: validate generic-high coordinate terminal

--- scripts/check_riemann_g2_current_direct_cut.sh
+++ scripts/check_riemann_g2_current_direct_cut.sh
@@ -26,6 +26,7 @@ FILES=(
   DASHI/Analysis/RiemannG2ConstructiveNegativeRHCompletionExact.agda
   DASHI/Analysis/RiemannG2ClayTerminalOneLeafCutExact.agda
   DASHI/Analysis/RiemannG2ClayTerminalCoordinateRefinedExact.agda
+  DASHI/Analysis/RiemannG2ClayTerminalGenericHighCoordinateExact.agda
   DASHI/Analysis/RiemannG2CurrentDirectOneLeafFrontierExact.agda
   DASHI/Analysis/RiemannG2FinalCutIntrospectionExact.agda
   DASHI/Analysis/Everything.agda
