8f71e1a0ac7f5f01eca24d124194b61ae410f47d
Johl Brown <johlbrown@gmail.com>
2026-09-09 19:20:12 +1000

    Validate photosynthetic optics cross-pollination

--- scripts/check_textile_physics.sh
+++ scripts/check_textile_physics.sh
@@ -13,9 +13,11 @@ AGDA_STDLIB="${AGDA_STDLIB:-/usr/share/agda/lib/stdlib}"
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Optics/OpticalSurfaceManufacturingToleranceExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Optics/LESOpticalNumericalWitnessCrossPollinationExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Optics/LESOpticalSurrogateEscalationExact.agda
-"$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Architecture/ASMLSolarOpticalRealisationCrossPollinationExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Environment/SolarOpticalSiteFibreCrossPollinationExact.agda
-"$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/SolarOpticalCrossPollinationValidation.agda
+"$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Environment/PlantHydraulicAtmosphereCarbonCouplingExact.agda
+"$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Environment/ConstitutiveHydrologyPlantCalibrationExact.agda
+"$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Environment/PhotosyntheticLightTransportCrossPollinationExact.agda
+"$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/PhotosyntheticOpticalCrossPollinationValidation.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Textile/TextileMechanicalDimensionExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Textile/TextileMechanicalFibreExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Textile/LinearElasticTextileLawExact.agda
