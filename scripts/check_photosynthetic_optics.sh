c36ff9fa509842794a21cab174bdca03064d2e4a
Johl Brown <johlbrown@gmail.com>
2026-09-09 23:19:39 +1000

    Compile dashiRTX photosynthetic adaptive transport bridge

--- scripts/check_photosynthetic_optics.sh
+++ scripts/check_photosynthetic_optics.sh
@@ -11,6 +11,8 @@ AGDA_STDLIB="${AGDA_STDLIB:-/usr/share/agda/lib/stdlib}"
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Environment/CanopySpectralRadiativeTransferExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Environment/PhotosyntheticLightOptimizationExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Environment/PhotosyntheticAssimilationValidationExact.agda
+"$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Optics/DashiRTXAdaptiveTransportSourceAtlasExact.agda
+"$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Environment/DashiRTXPhotosyntheticAdaptiveTransportCrossPollinationExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Optics/InverseCausticNumericalProducerExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Physics/Optics/LESOpticalNumericalWitnessCrossPollinationExact.agda
 "$AGDA_BIN" -i . -i "$AGDA_STDLIB" DASHI/Architecture/ASMLSolarOpticalRealisationCrossPollinationExact.agda
