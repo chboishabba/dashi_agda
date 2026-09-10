11853a1c0495c08304461a9d6018cfc16f133e5b
Johl Brown <johlbrown@gmail.com>
2026-09-10 10:22:31 +1000

    RH: validate generic near-upper and cellwise finite routes

--- scripts/check_riemann_g2_current_direct_cut.sh
+++ scripts/check_riemann_g2_current_direct_cut.sh
@@ -12,6 +12,8 @@ FILES=(
   DASHI/Analysis/RiemannG2ConcreteCertificateLiteralEnumerationExact.agda
   DASHI/Analysis/RiemannG2ConcreteScalarExecutionFrontierExact.agda
   DASHI/Analysis/RiemannG2FinalCarrierFiniteSumCertificateExact.agda
+  DASHI/Analysis/RiemannG2GenericNearUpperClusterResponseCompilerExact.agda
+  DASHI/Analysis/RiemannG2LiteralCellwiseNearUpperExact.agda
   DASHI/Analysis/RiemannG2DirectClusterResponseContradictionExact.agda
   DASHI/Analysis/RiemannG2CertifiedNearUpperClusterResponseCompilerExact.agda
   DASHI/Analysis/RiemannG2CertifiedClusterLowerEnvelopeCompilerExact.agda
