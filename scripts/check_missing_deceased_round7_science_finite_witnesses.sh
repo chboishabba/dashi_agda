f20ff610cdc13a93d7da0b77fc40316b04cd4a92
Johl Brown <johlbrown@gmail.com>
2026-09-12 13:47:31 +1000

    test: require remaining finite science witnesses

--- scripts/check_missing_deceased_round7_science_finite_witnesses.sh
+++ scripts/check_missing_deceased_round7_science_finite_witnesses.sh
@@ -10,6 +10,14 @@ required_files=(
   "DASHI/Physics/SpaceWeather/ZhangXiaoxinGeomagneticForecastFiniteWitnessExact.agda"
   "DASHI/Control/ZhangDaibingUAVControlFiniteWitnessExact.agda"
   "DASHI/Biology/LiMinyongPhotopharmacologyFiniteWitnessExact.agda"
+  "DASHI/Physics/Nuclear/LeBlancFSPICFiniteWitnessExact.agda"
+  "DASHI/Physics/Spectroscopy/MaiwaldActionSpectroscopyFiniteWitnessExact.agda"
+  "DASHI/Physics/Planetary/HicksSmallBodyPhotometryFiniteWitnessExact.agda"
+  "DASHI/Biology/JasonThomasSignallingFiniteWitnessExact.agda"
+  "DASHI/Physics/ExoticGravity/NingLiYBCOApparatusComparisonFiniteWitnessExact.agda"
+  "DASHI/GameTheory/FengYangheClassificationFiniteWitnessExact.agda"
+  "DASHI/Physics/Materials/FangDainingInverseDesignFiniteWitnessExact.agda"
+  "DASHI/Physics/Aerospace/YanHongThermalExcitationFiniteWitnessExact.agda"
   "DASHI/Culture/MissingDeceasedTwentyScientistRound7FiniteWitnessProgressExact.agda"
   "DASHI/Culture/MissingDeceasedTwentyScientistScienceFiniteWitnessBidiExact.agda"
 )
@@ -24,22 +32,20 @@ grep -q 'finiteLiuDSMMWitness' "$branch_root/DASHI/ComputerScience/LiuDonghaoDSM
 grep -q 'finiteZhangXiaoxinForecastWitness' "$branch_root/DASHI/Physics/SpaceWeather/ZhangXiaoxinGeomagneticForecastFiniteWitnessExact.agda"
 grep -q 'finiteZhangDaibingControlWitness' "$branch_root/DASHI/Control/ZhangDaibingUAVControlFiniteWitnessExact.agda"
 grep -q 'finiteLiMinyongPhotopharmWitness' "$branch_root/DASHI/Biology/LiMinyongPhotopharmacologyFiniteWitnessExact.agda"
+grep -q 'finiteLeBlancFSPICWitness' "$branch_root/DASHI/Physics/Nuclear/LeBlancFSPICFiniteWitnessExact.agda"
+grep -q 'finiteMaiwaldActionWitness' "$branch_root/DASHI/Physics/Spectroscopy/MaiwaldActionSpectroscopyFiniteWitnessExact.agda"
+grep -q 'finiteHicksPhotometryWitness' "$branch_root/DASHI/Physics/Planetary/HicksSmallBodyPhotometryFiniteWitnessExact.agda"
+grep -q 'finiteThomasSignallingWitness' "$branch_root/DASHI/Biology/JasonThomasSignallingFiniteWitnessExact.agda"
+grep -q 'finiteNingApparatusWitness' "$branch_root/DASHI/Physics/ExoticGravity/NingLiYBCOApparatusComparisonFiniteWitnessExact.agda"
+grep -q 'finiteFengClassificationWitness' "$branch_root/DASHI/GameTheory/FengYangheClassificationFiniteWitnessExact.agda"
+grep -q 'finiteFangInverseDesignWitness' "$branch_root/DASHI/Physics/Materials/FangDainingInverseDesignFiniteWitnessExact.agda"
+grep -q 'finiteYanThermalExcitationWitness' "$branch_root/DASHI/Physics/Aerospace/YanHongThermalExcitationFiniteWitnessExact.agda"
 grep -q 'round7ScientificCohortCount = 20' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistRound7FiniteWitnessProgressExact.agda"
 grep -q 'round7EveryScientistTouched = true' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistRound7FiniteWitnessProgressExact.agda"
 grep -q 'finiteWitnessDoesNotCreateHistoricalDeployment = false' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistRound7FiniteWitnessProgressExact.agda"
 grep -q 'MissingDeceasedTwentyScientistRound7FiniteWitnessProgressExact' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistRoundRobinEverything.agda"
 
-for import_name in \
-  ChenShumingGraphHardwareVerificationFiniteWitnessExact \
-  ZhouGuangyuanPolyimideAerogelFiniteWitnessExact \
-  LiuDonghaoDSMMFiniteWitnessExact \
-  ZhangXiaoxinGeomagneticForecastFiniteWitnessExact \
-  ZhangDaibingUAVControlFiniteWitnessExact \
-  LiMinyongPhotopharmacologyFiniteWitnessExact; do
-  grep -q "$import_name" "$branch_root/DASHI/Culture/MissingDeceasedChineseScienceImplementationEverything.agda"
-done
-
-grep -q 'finiteScienceWitnessBindingsCount = 6' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistScienceFiniteWitnessBidiExact.agda"
+grep -q 'finiteScienceWitnessBindingsCount = 14' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistScienceFiniteWitnessBidiExact.agda"
 grep -q 'syntheticFiniteWitnessCannotPaySourceReplication = false' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistScienceFiniteWitnessBidiExact.agda"
 grep -q 'finiteWitnessCannotPayHistoricalDeployment = false' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistScienceFiniteWitnessBidiExact.agda"
 grep -q 'MissingDeceasedTwentyScientistScienceFiniteWitnessBidiExact' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistRoundRobinEverything.agda"
