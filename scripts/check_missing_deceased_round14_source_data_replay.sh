5730edc72208acc0d72dc5a4a4a812a6eb69becb
Johl Brown <johlbrown@gmail.com>
2026-09-13 18:14:03 +1000

    test: require round14 source-data BIDI adapter

--- scripts/check_missing_deceased_round14_source_data_replay.sh
+++ scripts/check_missing_deceased_round14_source_data_replay.sh
@@ -10,6 +10,7 @@ required_files=(
   "DASHI/Biology/JasonThomasAssayDataReplayExact.agda"
   "DASHI/Control/ZhangDaibingControlDataReplayExact.agda"
   "DASHI/GameTheory/FengYangheClassificationDataReplayExact.agda"
+  "DASHI/Culture/MissingDeceasedTwentyScientistScienceSourceDataReplayBidiExact.agda"
   "DASHI/Culture/MissingDeceasedTwentyScientistRound14SourceDataReplayProgressExact.agda"
 )
 
@@ -23,11 +24,15 @@ grep -q 'hicksPhotometryDataReplay' "$branch_root/DASHI/Physics/Planetary/HicksS
 grep -q 'thomasAssayDataReplay' "$branch_root/DASHI/Biology/JasonThomasAssayDataReplayExact.agda"
 grep -q 'zhangDaibingControlDataReplay' "$branch_root/DASHI/Control/ZhangDaibingControlDataReplayExact.agda"
 grep -q 'fengClassificationDataReplay' "$branch_root/DASHI/GameTheory/FengYangheClassificationDataReplayExact.agda"
+grep -q 'sourceDataReplayBindingsCount = 6' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistScienceSourceDataReplayBidiExact.agda"
+grep -q 'numericSourceDataAndBlockedRowsRemainDistinct = true' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistScienceSourceDataReplayBidiExact.agda"
+grep -q 'sourceDataBindingDoesNotPayHistoricalUse = false' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistScienceSourceDataReplayBidiExact.agda"
 grep -q 'round14ScientificCohortCount = 20' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistRound14SourceDataReplayProgressExact.agda"
 grep -q 'round14EveryScientistTouched = true' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistRound14SourceDataReplayProgressExact.agda"
 grep -q 'round14SourceDataReplayPromotionCount = 6' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistRound14SourceDataReplayProgressExact.agda"
 grep -q 'sourceDataReplayDoesNotPayHistoricalDeployment = false' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistRound14SourceDataReplayProgressExact.agda"
 grep -q 'sourceDataReplayDoesNotPayCustody = false' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistRound14SourceDataReplayProgressExact.agda"
+grep -q 'MissingDeceasedTwentyScientistScienceSourceDataReplayBidiExact' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistRoundRobinEverything.agda"
 grep -q 'MissingDeceasedTwentyScientistRound14SourceDataReplayProgressExact' "$branch_root/DASHI/Culture/MissingDeceasedTwentyScientistRoundRobinEverything.agda"
 
 echo 'Round-14 source-data replay static contract: OK'
