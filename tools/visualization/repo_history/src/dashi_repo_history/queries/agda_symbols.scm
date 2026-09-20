7c9b0b95aa14eece1754dd61eb3f316ac0cb30bc
Johl Brown <johlbrown@gmail.com>
2026-09-21 00:35:18 +1000

    Abstract language extraction: tools/visualization/repo_history/src/dashi_repo_history/queries/agda_symbols.scm

---
+++ tools/visualization/repo_history/src/dashi_repo_history/queries/agda_symbols.scm
@@ -0,0 +1,9 @@
+(module_name) @module_name
+(data_name) @data_name
+(record_name) @record_name
+(field_name) @field_name
+(function_name) @function_name
+(typed_binding) @typed_binding
+(untyped_binding) @untyped_binding
+(qid) @reference
+(id) @reference
