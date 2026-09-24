a4629a81cc4038e38816cf7568dcea642684307c
Johl Brown <johlbrown@gmail.com>
2026-09-24 23:29:23 +1000

    GRQFT: wire terminal finite-measure ten-readout owner

--- scripts/check_grqft_postmerge_localization.sh
+++ scripts/check_grqft_postmerge_localization.sh
@@ -66,6 +66,7 @@ files=(
   DASHI/Physics/Foundations/CMP119TenLiteralDensitySourceReadoutsExact.agda
   DASHI/Physics/Foundations/CMP119LiteralFiniteMeasureStressSourceConstructorExact.agda
   DASHI/Physics/Foundations/CMP119LiteralFiniteMeasureDensityAnchorConstructorExact.agda
+  DASHI/Physics/Foundations/CMP119TenLiteralFiniteMeasureReadoutsExact.agda
   DASHI/Physics/Foundations/CMP119PinnedStressMetricRepresentationBridgeExact.agda
   DASHI/Physics/Foundations/CMP119PinnedStressMetricRepresentationBridgeValidation.agda
   DASHI/Physics/Closure/DrellYanRatioCancellationBoundaryExact.agda
