85e653fdd4615aa9a385b724d8fce9f32b5e6a26
Johl Brown <johlbrown@gmail.com>
2026-09-24 22:56:44 +1000

    GRQFT: wire literal-density ten-readout reduction

--- scripts/check_grqft_postmerge_localization.sh
+++ scripts/check_grqft_postmerge_localization.sh
@@ -63,6 +63,7 @@ files=(
   DASHI/Physics/Foundations/CMP119ConcreteTenSlotCrossNumeratorCandidateExact.agda
   DASHI/Physics/Foundations/CMP119ConcreteTenSlotD1SourceWeldExact.agda
   DASHI/Physics/Foundations/CMP119TenActualSourceReadoutsExact.agda
+  DASHI/Physics/Foundations/CMP119TenLiteralDensitySourceReadoutsExact.agda
   DASHI/Physics/Foundations/CMP119PinnedStressMetricRepresentationBridgeExact.agda
   DASHI/Physics/Foundations/CMP119PinnedStressMetricRepresentationBridgeValidation.agda
   DASHI/Physics/Closure/DrellYanRatioCancellationBoundaryExact.agda
