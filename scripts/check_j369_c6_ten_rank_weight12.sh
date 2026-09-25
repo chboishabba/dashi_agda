cbb354798b6d5e419fde49ee1b535bce3e26f51d
Johl Brown <johlbrown@gmail.com>
2026-09-25 10:10:07 +1000

    CI guard: include SSP15 prime/internal fibre

--- scripts/check_j369_c6_ten_rank_weight12.sh
+++ scripts/check_j369_c6_ten_rank_weight12.sh
@@ -8,6 +8,7 @@ targets=(
   DASHI/Moonshine/JInvariant369NeutralCuspRelationCrossPollinationExact.agda
   DASHI/Moonshine/JInvariant369SSP15SignedFRACTRANBranchExact.agda
   DASHI/Moonshine/JInvariant369OggAddressSSP15NoGoExact.agda
+  DASHI/Moonshine/JInvariant369SSP15PrimeInternalFibreExact.agda
 )
 
 for target in "${targets[@]}"; do
@@ -45,3 +46,8 @@ grep -q 'p2AndP11HaveSameAddressCoarse10' "${targets[5]}"
 grep -q 'addressModeNever09' "${targets[5]}"
 grep -q 'modePreservingOggInternalBijectionImpossible' "${targets[5]}"
 grep -q 'chosenCarrierBijectionDerivedFromAddressLaw' "${targets[5]}"
+
+grep -q 'primeInternalCountArithmetic' "${targets[6]}"
+grep -q 'chosenGaugeAtP71IsNotNeutral' "${targets[6]}"
+grep -q 'everyPrimeHasEveryInternalLane' "${targets[6]}"
+grep -q 'chosenGaugeExhaustsSemanticCarrier' "${targets[6]}"
